1e931a9f499801799203f265e56826e03b045f05
Team SaaSBox <team@saasbox.net>
2022-05-21 12:58:52 -0700

    Integrated popup configurator for demo and enhancements
    Added a custom menu to user-v2 which is popup configurator page.
    Added proper highlighting for top menu items in tailwind based
    admin page.

--- assets/javascripts/dashboard.js
+++ assets/javascripts/dashboard.js
@@ -21,7 +21,35 @@ let parent = link.parents(".topmenu");
 
 // This keeps the submenu open while a submenu link (or the top menu item) is selected.
 parent.attr("x-data", '\{ isExpanded: true \}');
-
+ $('a[href*="ABC"]')
 // Add highlight classes to it.
 link.removeClass("text-indigo-400 focus_bg-indigo-700 hover_text-white hover_bg-indigo-700");
 link.addClass("text-white bg-indigo-900");
+
+// If current path is an admin path, highlight both /app/start and /app/admin
+// If current path is a user path, highlight both links with /app/start and /app/user
+// else highlight the matching link. (e.g. /app/xyz)
+
+// For top navbar based template link highlighting with /app/start => either user or admin based on current user.
+let toplink = $(`a[href*="${paths}"]`);
+let startlink = $(`a[href*="/app/start"]`);
+let userlink = $(`a[href*="/app/user"]`);
+let adminlink = $(`a[href*="/app/admin"]`);
+let profile = $("#profile").attr("data-profile");
+
+if (paths == "/app/user" || paths == "/app/admin") {
+    startlink.addClass("border-indigo-500 border-b-2");
+}
+
+
+
+toplink.addClass("border-indigo-500 border-b-2");
+
+if (profile == "user") {
+ userlink.addClass("border-indigo-500 border-b-2");
+}
+if (profile == "admin") {
+ console.log("Calling add class to admin")
+ adminlink.addClass("border-indigo-500 border-b-2");
+}
+console.log("Profile:", profile)
